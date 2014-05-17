90bfb3477f1824ea69dcea66befc22944e491082
jungrok5 <jungrok5@gmail.com>
2014-05-17 15:20:00 +0900

    #2 upload
    #2 upload

---
+++ SpiderNET #2/Server/ExampleServer/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExampleServer.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        public string Echo(string data)
+        {
+            return data;
+        }
+    }
+}
